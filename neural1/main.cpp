7d485332e25153ac2ab2c2a36141d040a60c3891
baguettio <61667298+baguettio@users.noreply.github.com>
2023-10-23 12:00:06 +0000

    removed unneccesasry paramters from templates and removed some obselete checks, added matrix subtraction, hadmard product and transposistion

--- neural1/main.cpp
+++ neural1/main.cpp
@@ -1,18 +1,15 @@
 #include <iostream> 
 #include <array>
 //REMEMBER MATRIX ADD IS NOT REPORTING DIMENSION ERRORS PROPERLY
-template <std::size_t Rows1, std::size_t Cols1, std::size_t Rows2, std::size_t Cols2, std::size_t Rows3, std::size_t Cols3> //template so that out matrix functions can take std::arrays of all sizes
-void matrixMultiply(const std::array<std::array<float, Cols1>, Rows1>& matrix1, const std::array<std::array<float, Cols2>, Rows2>& matrix2,  std::array<std::array<float, Cols3>, Rows3>& matrix3) {
+
+template <std::size_t Rows1, std::size_t Cols1, std::size_t Rows2, std::size_t Cols2> 
+void matrixMultiply(const std::array<std::array<float, Cols1>, Rows1>& matrix1, const std::array<std::array<float, Cols2>, Rows2>& matrix2,  std::array<std::array<float, Cols2>, Rows1>& matrix3) {
     if (Cols1 != Rows2)
     {
         std::cout << "Cannot multiply matrices of these dimensions" << std::endl;
         return;
     }
-    else if (Rows3 != Rows1 || Cols3 != Cols2)
-    {
-        std::cout << "Result matrix is of incorrect dimensions" << std::endl;
-        return;
-    }
+
     for(std::size_t i = 0; i < Rows1; i++)
     {
         for(std::size_t j = 0; j < Cols2; j++)
@@ -26,18 +23,42 @@ void matrixMultiply(const std::array<std::array<float, Cols1>, Rows1>& matrix1, 
         }
     }
 }
-template <std::size_t Rows1, std::size_t Cols1, std::size_t Rows2, std::size_t Cols2, std::size_t Rows3, std::size_t Cols3>
-void matrixAdd(const std::array<std::array<float, Cols1>, Rows1>& matrix1, const std::array<std::array<float, Cols2>, Rows2>& matrix2, std::array<std::array<float, Cols3>, Rows3>& resultMatrix) {
-    if(Cols1 != Cols2 || Cols1 != Cols3 || Rows1 != Rows2 || Rows1 != Rows3){
-        std::cout << "Invalid dimensions for matrix addition" << std::endl;
-    }
-    
+//since all three matrices should be of the same size for addition subtraction and the hadmard product, we only need a template for one of them (passing a matrix of incorrect dimensions will result in a runtime error)
+template <std::size_t Rows1, std::size_t Cols1>
+void matrixAdd(const std::array<std::array<float, Cols1>, Rows1>& matrix1, const std::array<std::array<float, Cols1>, Rows1>& matrix2, std::array<std::array<float, Cols1>, Rows1>& resultMatrix) {
     for (std::size_t i = 0; i < Rows1; i++) {
         for (std::size_t j = 0; j < Cols1; j++) {
             resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
         }
     }
 }
+template <std::size_t Rows1, std::size_t Cols1>
+void matrixSubtract(const std::array<std::array<float, Cols1>, Rows1>& matrix1, const std::array<std::array<float, Cols1>, Rows1>& matrix2, std::array<std::array<float, Cols1>, Rows1>& resultMatrix) {
+    for (std::size_t i = 0; i < Rows1; i++) {
+        for (std::size_t j = 0; j < Cols1; j++) {
+            resultMatrix[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+}
+template <std::size_t Rows1, std::size_t Cols1>
+void hadmardProduct(const std::array<std::array<float, Cols1>, Rows1>& matrix1, const std::array<std::array<float, Cols1>, Rows1>& matrix2, std::array<std::array<float, Cols1>, Rows1>& resultMatrix) {
+    
+    for (std::size_t i = 0; i < Rows1; i++) {
+        for (std::size_t j = 0; j < Cols1; j++) {
+            resultMatrix[i][j] = matrix1[i][j] * matrix2[i][j];
+        }
+    }
+}
+//and for transposition, we still only need one template but the second matrix will be of size cols1 x rows1
+template <std::size_t Rows1, std::size_t Cols1>
+void transpose(const std::array<std::array<float, Cols1>, Rows1>& matrix1, std::array<std::array<float, Rows1>, Cols1>& resultMatrix) {
+    for (std::size_t i = 0; i < Rows1; i++) {
+        for (std::size_t j = 0; j < Cols1; j++) {
+            resultMatrix[j][i] = matrix1[i][j];
+        }
+    }
+}
+
 int main(){
     constexpr size_t inputNeurons = 784, hiddenNeurons = 30, outputNeurons = 10;
 
@@ -59,7 +80,7 @@ int main(){
     //hyperparameters
     size_t epochs = 10; //number of times we iterate through all of the training data
     size_t batchSize = 128; //how many training examples we look at before we adjust the parameters
-    float learningRate = 0.01; //how much we adjust the parameters 
+    float learningRate = 0.01; //how much we adjust the parameters by
 
 
     constexpr size_t Rows1 = 2, Cols1 = 3, Rows2 = 3, Cols2 = 2;
@@ -69,7 +90,7 @@ int main(){
         std::array<float, Cols1>{4, 5, 6}
     };
 
-    std::array<std::array<float, Cols1>, Rows1> matrix2 = {
+    std::array<std::array<float, Cols2>, Rows1> matrix2 = {
         std::array<float, Cols1>{7, 8},
         std::array<float, Cols1>{9, 10},
     };
@@ -86,12 +107,5 @@ int main(){
     }
 
     return 0;
-
-
-
-    return 0;
-
-
-
 
 }
